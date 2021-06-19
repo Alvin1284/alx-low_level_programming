c818bdb7eef3afba57521d21c07e6a09a87dded7
Alvin Waweru <alvinwaweru43@gmail.comgit config --global user.email alvinwaweru43@gmail.com>
2021-06-19 16:55:43 -0700

    programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+/**
+ * main - Entry point
+ *
+ * puts - Prints the passed string with a new line
+ * 
+ * return: Always 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
+}
