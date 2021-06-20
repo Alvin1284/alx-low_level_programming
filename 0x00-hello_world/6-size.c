aa35a6cba2e088f48994c411f8e21fdb0ca45a77
Alvin Waweru <alvinwaweru43@gmail.comgit config --global user.email alvinwaweru43@gmail.com>
2021-06-20 12:39:21 -0700

    programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,9 +13,12 @@ int intType;
 float floatType;
 double doubleType;
 char charType;
-printf("Size of int: %zu bytes\n", sizeof(intType));
-printf("Size of float: %zu bytes\n", sizeof(floatType));
-printf("Size of double: %zu bytes\n", sizeof(doubleType));
+long int longType;
+long long int intlongType;
 printf("Size of char: %zu bytes\n", sizeof(charType));
+printf("Size of an int: %zu bytes\n", sizeof(intType));
+printf("Size of long int: %zu bytes\n", sizeof(longType);
+printf("Size of long long int: %zu bytes\n", sizeof(intlongType);
+printf("Size of float: %zu bytes\n", sizeof(floatType));
 return (0);
 }
