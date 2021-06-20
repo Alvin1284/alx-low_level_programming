659de2fff582424605e056e94d42638d35191ac0
Alvin Waweru <alvinwaweru43@gmail.comgit config --global user.email alvinwaweru43@gmail.com>
2021-06-19 17:33:09 -0700

    programming

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+/**
+ *main-prints string
+ *followed by a new line
+ * Return: always (0)
+ */
+int main(void)
+{printf("\"with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
